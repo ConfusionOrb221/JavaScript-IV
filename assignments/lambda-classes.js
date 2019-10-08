4349a4e75b15476121ae066465634676b3adabd9
ConfusionOrb221 <nate1898@gmail.com>
2019-10-07 22:07:10 -0400

    changed the prototypes to es6 classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,26 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attrs){
+        this.name = attrs.name;
+        this.age = attrs.age;
+        this.location = attrs.location;
+    }
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+class instructor extends person{
+    constructor(attrs){
+        super(attrs);
+        this.speciality = attrs.speciality;
+        this.favLanguage = attrs.favLanguage;
+        this.catchPhrase = attrs.catchPhrase;
+    }
+    demo(string){
+        
+    }
+    grade(object, string){
+
+    }
+}
