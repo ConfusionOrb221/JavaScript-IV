c45ce68fd9b722cdb1cb17978115949ab5b66b3a
ConfusionOrb221 <nate1898@gmail.com>
2019-10-07 22:32:28 -0400

    Created all the classes for each object and started to invoke those functions

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,8 +9,7 @@ class Person {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
     }
 }
-
-class instructor extends person{
+class Instructor extends Person{
     constructor(attrs){
         super(attrs);
         this.speciality = attrs.speciality;
@@ -18,9 +17,85 @@ class instructor extends person{
         this.catchPhrase = attrs.catchPhrase;
     }
     demo(string){
-        
+        console.log(`Today we are learning about ${string}`);
     }
     grade(object, string){
-
+        console.log(`${object.name} receieves a perfect score on ${string}`);
     }
 }
+
+class Student extends Person{
+    constructor(attrs){
+        super(attrs);
+        this.previousBackground = attrs.previousBackground;
+        this.className = attrs.className;
+        this.favSubjects = attrs.favSubjects;
+    }
+    listSubjects(){
+        this.favSubjects.array.forEach(i => {
+            console.log(i);
+        });
+    }
+    PRAssignment(string){
+        console.log(`${this.name} has submitted a PR for ${string}`);
+    }
+    sprintChallenge(string){
+        console.log(`${this.name} has begun sptring challenge on ${string}`);
+    }
+}
+
+class ProjectManager extends Instructor{
+    constructor(attrs){
+        super(attrs);
+        this.gradClassName = attrs.gradClassName;
+        this.favInstructor = attrs.favInstructor;
+    }
+    standUp(string){
+        console.log(`${this.name} announced to @${string} standy times!`);
+    }
+    debugsCode(object, string){
+        console.log(`${this.name} debugs ${object.name}'s code on ${string}`);
+    }
+}
+
+const lucas = new Instructor({
+    name: 'Lucas',
+    location: 'North Carolina',
+    age: 23,
+    favLanguage: 'Ruby',
+    speciality: 'Back-End',
+    catchPhrase: 'Gamer Time'
+});
+const todd = new Instructor({
+    name: 'Todd',
+    location: 'California',
+    age: 30,
+    favLanguage: 'JS',
+    speciality: 'Front-end',
+    catchPhrase: 'blip-bloop'
+});
+const nathan = new Student({
+    name: 'Nathan',
+    location: 'Brockport',
+    age: 23,
+    previousBackground: 'C#',
+    className: 'WPT11',
+    favSubjects: ['HTML, CSS, C#']
+});
+const sam = new Student({
+    name: 'Sam',
+    location: 'New York',
+    age: 23,
+    previousBackground: 'HTML',
+    className: 'WPT11',
+    favSubjects: ['HTML, CSS, JS']
+});
+
+console.log(lucas);
+lucas.speak();
+lucas.demo('Ruby');
+lucas.grade(nathan, 'Ruby - I');
+console.log(todd);
+todd.speak();
+todd.demo('JS');
+todd.grade(sam, 'JS - I');
